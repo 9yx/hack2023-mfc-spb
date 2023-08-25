35583df826a32240ea20f435d94ecfc86239be5a
mrv <mrv@ingins.ru>
2023-08-26 04:53:40 +0500

    wip

--- main2.py
+++ main2.py
@@ -1,3 +1,5 @@
+import string
+
 import pandas as pd
 
 import numpy as np
@@ -9,12 +11,41 @@ from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, GenerationConfig
 
 def generate(prompt):
     data = tokenizer(f"{prompt}", return_tensors="pt").to(model.device)
-    output_ids = model.generate(
+    output_ids = fredT5Large.generate(
         **data,
         generation_config=generation_config
     )[0]
     out = tokenizer.decode(output_ids.tolist(), skip_special_tokens=True)
     return out.replace('<extra_id_0>', '').strip()
+
+
+def lowercase_text(text):
+    return text.lower()
+
+
+def remove_extra_spaces(text):
+    return " ".join(text.split())
+
+
+def remove_punctuation(text):
+    translation_table = str.maketrans("", "", string.punctuation)
+    return text.translate(translation_table)
+
+
+def get_best(query, K=3):
+    query_embedding = model.encode([query])
+    distances = cdist(query_embedding, faq_embeddings, "cosine")[0]
+    ind = np.argsort(distances, axis=0)
+    print("\n" + query)
+    for c, i in list(zip(distances[ind], ind))[:K]:
+        # print(c, question_dataset[i], answer_dataset.loc[i], sep="\t")
+        question = remove_punctuation(lowercase_text(remove_extra_spaces(remove_punctuation(question_dataset[i])))).replace("\n", "")
+        answer = lowercase_text(remove_extra_spaces(remove_punctuation(answer_dataset.loc[i]))).replace("\n", "")
+        text = f'''Вопрос: {question}?  Ответ: {answer}'''
+        question = query
+        prompt = '''<SC6>Текст: {}\nВопрос: {}\nОтвет: <extra_id_0>'''.format(text, question)
+        print(prompt)
+        print(generate(prompt))
 
 
 if __name__ == '__main__':
@@ -24,25 +55,9 @@ if __name__ == '__main__':
     tokenizer = AutoTokenizer.from_pretrained("Den4ikAI/FRED-T5-LARGE_text_qa")
     fredT5Large = AutoModelForSeq2SeqLM.from_pretrained("Den4ikAI/FRED-T5-LARGE_text_qa").to(device)
     fredT5Large.eval()
-
     question_dataset = pd.read_excel('train_dataset_Датасет.xlsx', 'Лист1')['QUESTION'].astype(str).tolist()  # [:-50]
     answer_dataset = pd.read_excel('train_dataset_Датасет.xlsx', 'Лист1')['ANSWER'].astype(str)
-    model = SentenceTransformer('intfloat/multilingual-e5-large', device=device)
-
+    # model = SentenceTransformer('intfloat/multilingual-e5-large', device=device)
+    model = SentenceTransformer('sentence-transformers/quora-distilbert-multilingual', device=device)
     faq_embeddings = model.encode(question_dataset, normalize_embeddings=True)
-
-
-    def get_best(query, K=3):
-        query_embedding = model.encode([query])
-        distances = cdist(query_embedding, faq_embeddings, "cosine")[0]
-        ind = np.argsort(distances, axis=0)
-        print("\n" + query)
-        for c, i in list(zip(distances[ind], ind))[:K]:
-            print(c, question_dataset[i], answer_dataset.loc[i], sep="\t")
-            text = f'''Вопрос: {question_dataset[i]}?  Ответ: {answer_dataset.loc[i]}'''
-            question = query
-            prompt = '''<SC6>Текст: {}\nВопрос: {}\nОтвет: <extra_id_0>'''.format(text, question)
-            print(generate(prompt))
-
-
-    get_best("Какие условия в услуге 33?", 3)
+    get_best("Какие условия в услуге 33?", 1)
