8f1875508116e8fdcc202d9b150447d03165bb8a
mrv <mrv@ingins.ru>
2023-08-27 00:44:00 +0500

    wip

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from fastapi import FastAPI, UploadFile
 from pydantic import BaseModel
 from scipy.spatial.distance import cdist
 from sentence_transformers import SentenceTransformer
-from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, GenerationConfig
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, GenerationConfig, AutoModelForSequenceClassification
 
 import model_like_service as ms
 
@@ -51,27 +51,38 @@ def get_best(query, K=3):
     text = ""
     regression = ms.order_with_like(list(zip(distances[ind], ind)))
     index = None
+
+    queryes = []
+    questions = []
+    answers = []
     for c, i in regression[:K]:
         question = simplify_text(question_dataset[i])
         answer = simplify_text(answer_dataset.loc[i])
         print(c, question, answer)
-        if c > 0.1:
+        if c > 0.035:
             continue
-        text += f'''Вопрос: {question}\nОтвет: {answer}\n'''
+        questions.append(question)
+        queryes.append(query)
+        answers.append(answer)
 
-        if index is None:
-            index = i
+    if len(answers) == 0:
+        return [-1, "Не знаю ответа"]
 
-    if text == "":
-        return "Не знаю ответа"
+    features = cross_encoder_tokenizer(queryes, answers, padding=True, truncation=True, return_tensors="pt")
+    cross_encoder_model.eval()
+    with torch.no_grad():
+        index = torch.argmax(cross_encoder_model(**features).logits).item()
+        text = f'''Вопрос: {questions[index]}\nОтвет: {answers[index]}\n'''
+        print("Выбрали вариант:", text)
+
     prompt = '''<SC6>Текст: {}\nВопрос: {}\nОтвет: <extra_id_0>'''.format(text.strip(), query)
     result = generate(prompt)
     return [index, result]
 
 
 # device = torch.cuda.current_device() if torch.cuda.is_available() and torch.cuda.mem_get_info()[0] >= 2*1024**3 else -1
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-#device = torch.device("cpu")
+#device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cpu")
 
 skipAiTraining = False
 # Проверка существования файла
@@ -94,6 +105,8 @@ question_dataset = None
 answer_dataset = None
 model = None
 faq_embeddings = None
+cross_encoder_model = None
+cross_encoder_tokenizer = None
 
 if not skipAiTraining:
     generation_config = GenerationConfig.from_pretrained("Den4ikAI/FRED-T5-LARGE_text_qa")
@@ -103,12 +116,16 @@ if not skipAiTraining:
     question_dataset = pd.read_excel('train_dataset_Датасет.xlsx', 'Лист1')['QUESTION'].astype(str).tolist()
     answer_dataset = pd.read_excel('train_dataset_Датасет.xlsx', 'Лист1')['ANSWER'].astype(str)
     # model = SentenceTransformer('intfloat/multilingual-e5-large', device=device)
+    # model = SentenceTransformer('Den4ikAI/sbert_large_mt_ru_retriever', device=device)
     model = SentenceTransformer('sentence-transformers/quora-distilbert-multilingual', device=device)
     faq_embeddings = model.encode(question_dataset, normalize_embeddings=True)
 
+    cross_encoder_model = AutoModelForSequenceClassification.from_pretrained('cross-encoder/mmarco-mMiniLMv2-L12-H384-v1')
+    cross_encoder_tokenizer = AutoTokenizer.from_pretrained('cross-encoder/mmarco-mMiniLMv2-L12-H384-v1')
+
 
 def process_query(query):
-    return get_best(query, 1)
+    return get_best(query, 3)
 
 
 class QADataModel(BaseModel):
